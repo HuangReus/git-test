8829620bf19500c4e1c96be3a044c7350884d287
huangxijun <huangxijun@huawei.com>
2022-03-23 21:45:28 +0800

    remote commit.

--- src/main/java/com/huangxijun/git.java
+++ src/main/java/com/huangxijun/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("hello hot-fix.");
         System.out.println("master.");
         System.out.println("fix.");
+        System.out.println("remote test.");
     }
 }
