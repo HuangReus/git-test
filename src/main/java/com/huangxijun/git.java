2c2127c75ce6b1087e0c831de02eab0c081a1986
huangxijun <huangxijun@huawei.com>
2022-03-23 17:06:47 +0800

    second commit.

--- src/main/java/com/huangxijun/git.java
+++ src/main/java/com/huangxijun/git.java
@@ -3,5 +3,6 @@ package com.huangxijun;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git.");
+        System.out.println("hello github.");
     }
 }
